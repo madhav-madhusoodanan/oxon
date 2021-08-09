0fcc49ada5a1d97c371bb85868ada872beb5b3ba
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-08-10 00:42:03 +0400

    added floating action button for cart redirect

--- screens/shop/Items.tsx
+++ screens/shop/Items.tsx
@@ -6,6 +6,7 @@ import {appSelector, appDispatch} from '../../store/hooks/hooks';
 import Item_base from '../../components/Item_display';
 import {RootStackParamList} from '../../RootStackParamList';
 import {StackScreenProps} from '@react-navigation/stack';
+import {FAB} from 'react-native-paper';
 
 const Items = ({navigation}: StackScreenProps<RootStackParamList, 'Items'>) => {
   const items = appSelector(state => state.items.available_items);
@@ -13,8 +14,13 @@ const Items = ({navigation}: StackScreenProps<RootStackParamList, 'Items'>) => {
   const Item_display = Item_base(navigation, dispatch);
 
   return (
-    <View style={style('flex_row justify_around')}>
+    <View style={style('flex_row justify_around h_full')}>
       <FlatList<Item> data={items} renderItem={Item_display} numColumns={2} />
+      <FAB
+        
+        icon="cart"
+        style={{position: 'absolute', margin: 16, right: 0, bottom: 0}}
+      />
     </View>
   );
 };
